436e093a351538e274296bbb6e579ab71d40a39e
Gustavo Alarcon <galarcon@meraki-s.com>
2020-09-15 19:47:00 -0500

    new text at link share

--- src/app/core/services/database.service.ts
+++ src/app/core/services/database.service.ts
@@ -17,7 +17,7 @@ import { Package } from '../models/package.model';
   providedIn: 'root'
 })
 export class DatabaseService {
-  public version: string = 'V1.1.36r';
+  public version: string = 'V1.1.37r';
   public isOpen: boolean = false;
   public isAdmin: boolean = false;
 
