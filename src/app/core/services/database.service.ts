c1e2e433b8d3ba669a07388ea4946d30f32d24d9
Junjiro <jobando@meraki-s.com>
2020-09-28 09:47:27 -0500

    deploy v1.1.38r

--- src/app/core/services/database.service.ts
+++ src/app/core/services/database.service.ts
@@ -17,7 +17,7 @@ import { Package } from '../models/package.model';
   providedIn: 'root'
 })
 export class DatabaseService {
-  public version: string = 'V1.1.37r';
+  public version: string = 'V1.1.38r';
   public isOpen: boolean = false;
   public isAdmin: boolean = false;
 
