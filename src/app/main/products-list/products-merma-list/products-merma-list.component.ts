9fb2fcdab0838314b0912bcbda9862ac993f7485
Junjiro <jobando@meraki-s.com>
2020-09-29 13:11:21 -0500

    merma and login
    -historial de merma terminado
    -Se arreglo login

--- src/app/main/products-list/products-merma-list/products-merma-list.component.ts
+++ src/app/main/products-list/products-merma-list/products-merma-list.component.ts
@@ -13,7 +13,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 
-import { Product } from 'src/app/core/models/product.model';
+import { MermaTransfer, MermaTransferWithProduct, Product } from 'src/app/core/models/product.model';
 import { ProductCreateEditComponent } from '../product-create-edit/product-create-edit.component';
 import { ProductEditPromoComponent } from '../product-edit-promo/product-edit-promo.component';
 import { ConfirmationDialogComponent } from '../../confirmation-dialog/confirmation-dialog.component';
@@ -21,6 +21,7 @@ import { Category } from 'src/app/core/models/category.model';
 import { User } from 'src/app/core/models/user.model';
 import { ProductTransferMermaComponent } from '../product-transfer-merma/product-transfer-merma.component';
 import { ProductMermaHistoryComponent } from '../product-merma-history/product-merma-history.component';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
@@ -33,7 +34,8 @@ export class ProductsMermaListComponent implements OnInit {
   //Forms
   categoryForm: FormControl;
   itemsFilterForm: FormControl;
-  promoFilterForm: FormControl;
+  historyForm: FormControl;
+  date: FormControl;
 
   //Table
   productsTableDataSource = new MatTableDataSource<Product>();
@@ -42,16 +44,26 @@ export class ProductsMermaListComponent implements OnInit {
     'unitDescription', /*'unitAbbreviation', */'unitWeight', /*'sellMinimum', 'alertMinimum',*/
     /*'realStock',*/ 'mermaStock', /*'virtualStock', 'published', */'actions'
   ]
+  mermaTableDataSource = new MatTableDataSource<MermaTransferWithProduct>();
+  mermaDisplayedColumns: string[] = [
+    'index', 'description', 'sku', 'category', 'unitDescription', 'unitWeight', 
+    'toMerma', 'quantity', 'date', 'user', 'observations', 'actions'
+  ]
 
   productsObservable$: Observable<Product[]>
   @ViewChild('productsPaginator', { static: false }) set content(paginator1: MatPaginator) {
     this.productsTableDataSource.paginator = paginator1;
   }
 
+  mermaObservable$: Observable<MermaTransferWithProduct[]>
+  @ViewChild('mermaPaginator', { static: false }) set contentMerma(paginator1: MatPaginator) {
+    this.mermaTableDataSource.paginator = paginator1;
+  }
+
   //Observables
   categoryObservable$: Observable<[any, Category[]]>
-  categoryList$: Observable<Category[]>
-  filter$: Observable<boolean>
+  categoryList$: Observable<Category[]>;
+  filter$: Observable<boolean>;
 
 
   //Variables
@@ -68,7 +80,9 @@ export class ProductsMermaListComponent implements OnInit {
     private dialog: MatDialog,
     public snackBar: MatSnackBar,
     private dbs: DatabaseService,
-    public auth: AuthService
+    public auth: AuthService,
+    private datePipe: DatePipe,
+
   ) { }
 
   ngOnInit(): void {
@@ -79,12 +93,25 @@ export class ProductsMermaListComponent implements OnInit {
   initForms() {
     this.categoryForm = this.fb.control("");
     this.itemsFilterForm = this.fb.control("");
-    this.promoFilterForm = this.fb.control(false);
+    this.historyForm = this.fb.control(false);
+
+    this.date = new FormControl({
+      begin: this.getCurrentMonthOfViewDate().from,
+      end: this.getCurrentMonthOfViewDate().to
+    });
   }
 
   initObservables() {
     this.productsTableDataSource.filterPredicate =
       (data: Product, filter: string) => {
+        let category = filter.trim().split('&+&')[0];   //category
+        let name = filter.trim().split('&+&')[1];       //product name
+        return (!!data.category.match(new RegExp(category, 'ig'))
+          && !!data.description.match(new RegExp(name, 'ig')))
+      }
+
+    this.mermaTableDataSource.filterPredicate =
+      (data: MermaTransferWithProduct, filter: string) => {
         let category = filter.trim().split('&+&')[0];   //category
         let name = filter.trim().split('&+&')[1];       //product name
         return (!!data.category.match(new RegExp(category, 'ig'))
@@ -118,21 +145,42 @@ export class ProductsMermaListComponent implements OnInit {
       .pipe(
         map(([categorySelected, itemsFormValue]) => {
           this.productsTableDataSource.filter = (categorySelected.length > 1 ? '' : categorySelected[0].name) + '&+&' + itemsFormValue;
+          this.mermaTableDataSource.filter = (categorySelected.length > 1 ? '' : categorySelected[0].name) + '&+&' + itemsFormValue;
           return true
         })
       )
 
     this.productsObservable$ = this.dbs.getProductsListValueChanges().pipe(
-      map(res => res.filter(el => !!el.mermaStock)),
       tap(res => {
-        this.productsTableDataSource.data = res.map(el => {
-          el['virtualStock$'] = this.dbs.getVirtualStock(el).pipe(
-            map(prod => prod.reduce((a, b) => a + b.quantity, 0))
-          );
+        let withMerma = res.filter(el => !!el.mermaStock)
+        this.productsTableDataSource.data = withMerma.map(el => {
+          // el['virtualStock$'] = this.dbs.getVirtualStock(el).pipe(
+          //   map(prod => prod.reduce((a, b) => a + b.quantity, 0))
+          // );
           return el
         })
       })
     )
+
+    this.mermaObservable$ = combineLatest([
+      this.productsObservable$,
+      this.date.valueChanges.pipe(startWith(this.date.value), switchMap(date => (
+        this.dbs.getMermaTransferHistoryDate(date)
+      )))
+    ]).pipe(map(([productList, mermaHistory])=>{
+
+
+      let productWithMerma: MermaTransferWithProduct[] = mermaHistory
+        .filter(el => !!productList.find(el2 => el.productId==el2.id))
+        .map(el => ({...productList.find(el2 => el.productId==el2.id), ...el}))
+
+
+      this.mermaTableDataSource.data = productWithMerma
+
+      return productWithMerma
+    }))
+
+
 
 
   }
@@ -189,54 +237,91 @@ export class ProductsMermaListComponent implements OnInit {
     }
   }
 
-  onTransferHistoryMerma(product: Product){
+  onTransferHistoryMerma(product: MermaTransferWithProduct){
     let dialogRef: MatDialogRef<ProductMermaHistoryComponent>;
     dialogRef = this.dialog.open(ProductMermaHistoryComponent, {
       data: {
-        product: { ...product },
+        product: { ...product, id: product.productId },
       }
     });
   }
 
+  getCurrentMonthOfViewDate(): { from: Date, to: Date } {
+    const date = new Date();
+    const fromMonth = date.getMonth();
+    const fromYear = date.getFullYear();
+
+    const actualFromDate = new Date(fromYear, fromMonth, 1);
+
+    const toMonth = (fromMonth + 1) % 12;
+    let toYear = fromYear;
+
+    if (fromMonth + 1 >= 12) {
+      toYear++;
+    }
+
+    const toDate = new Date(toYear, toMonth, 1);
+
+    return { from: actualFromDate, to: toDate };
+  }
+
   downloadXls(): void {
     let table_xlsx: any[] = [];
-    let headersXlsx = [
-      'Descripcion', 'SKU', 'Categoría', 'Precio',
-      'Descripción de Unidad', 'Abreviación', 'Peso (KG)', 'Stock Real', 'Mínimo de venta', 'Mínimio de alerta',
-      'Stock de merma', 'Stock Virtual', 'Publicado'
+    let headersXlsxProducts = [
+      'Descripcion', 'SKU', 'Categoría', 
+      'Descripción de Unidad', 'Peso (KG)', 'Merma'
+    ]
+    let headersXlsxMerma = [
+      'Descripcion', 'SKU', 'Categoría', 
+      'Descripción de Unidad', 'Peso (KG)', 'Dirección',
+      'Cantidad', 'Fecha', 'Usuario', 'Observaciones'
     ]
 
-    table_xlsx.push(headersXlsx);
+    if(!this.historyForm.value){
+      table_xlsx.push(headersXlsxProducts);
 
-    this.productsTableDataSource.filteredData.forEach(product => {
-      const temp = [
-        product.description,
-        product.sku,
-        product.category,
-        "S/." + product.price,
-        product.unit.description,
-        product.unit.abbreviation,
-        product.unit.weight,
-        product.realStock,
-        product.sellMinimum,
-        product.alertMinimum,
-        product.mermaStock,
-        0, //virtualStock
-        product.published ? "Sí" : "No"
-      ];
+      this.productsTableDataSource.filteredData.forEach(product => {
+        const temp = [
+          product.description,
+          product.sku,
+          product.category,
+          product.unit.description,
+          product.unit.weight,
+          product.mermaStock,
+        ];
 
-      table_xlsx.push(temp);
-    })
+        table_xlsx.push(temp);
+      })
+    } else {
+      table_xlsx.push(headersXlsxMerma);
+
+      this.mermaTableDataSource.data.forEach(product => {
+        const temp = [
+          product.description,
+          product.sku,
+          product.category,
+          product.unit.description,
+          product.unit.weight,
+          product.toMerma ? "De stock a Merma.":"De merma a Stock.",
+          product.quantity,
+          this.datePipe.transform(product.date['seconds'] * 1000, 'dd/MM/yyyy(hh:mm:ss)'),
+          product.user.displayName,
+          product.observations
+        ];
+
+        table_xlsx.push(temp);
+      })
+    }
 
     /* generate worksheet */
     const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(table_xlsx);
 
     /* generate workbook and add the worksheet */
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Lista_de_productos');
+    XLSX.utils.book_append_sheet(wb, ws, this.historyForm.value ? "Historial_de_merma":'Lista_de_productos');
 
     /* save to file */
-    const name = 'Lista_de_productos' + '.xlsx';
+    const name = (this.historyForm.value ? "Historial_de_merma":'Lista_de_productos') + '.xlsx';
     XLSX.writeFile(wb, name);
   }
 }
